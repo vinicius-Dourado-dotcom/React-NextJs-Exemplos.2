d314db95d9b69a21e7fc3847d6eb845e7f887c3c
vinicius-Dourado-dotcom <vidourado@hotmail.com>
2024-08-29 17:55:37 -0300

    Criado useState

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,9 +1,19 @@
+  import { useState } from 'react'
   import Head from 'next/head'
   
   import Title from '../src/components/title/Title'
   import Subtitle from '../src/components/subtitle/Subtitle'
+  import Button from '../src/components/button/Button'
+
+  
   
   function HomePage () {
+    const [click, setClick] = useState(0)
+    const handleClick = () => {
+      console.log("HANDLE CLICK")
+      setClick (click + 1)
+    }
+    
     return (
       <>
         <Head>
@@ -14,6 +24,8 @@
           <Subtitle text="Um subtítulo"/>
           <Title>Outro titulo</Title>
           <Subtitle text="Outro subtítulo"/>
+          <Button onClick={handleClick}>Botão Teste</Button>
+          <p>cliques: {click}</p>
         </div>
       </>
     )
