2bc39c0f1246e9937d99326b37b45a696b26b709
vinicius-Dourado-dotcom <vidourado@hotmail.com>
2024-08-29 20:28:36 -0300

    Criado Header

---
+++ pages/Exemplo.jsx
@@ -0,0 +1,16 @@
+import Header from '../src/components/header/Header'
+import Title from '../src/components/title/Title'
+import Subtitle from '../src/components/subtitle/Subtitle'
+
+function ExemploPage () {
+   return (
+     <>
+      <Header>
+       <Title>Café Dev</Title>
+       <Subtitle text="O melhor café para p programador"/>
+      </Header>
+     </>
+   )
+}
+
+export default ExemploPage 
---
+++ pages/outroexemplo.jsx
@@ -0,0 +1,17 @@
+import Header from '../src/components/header/Header'
+import Title from '../src/components/title/Title'
+import Subtitle from '../src/components/subtitle/Subtitle'
+
+function OutroExemploPage () {
+  return (
+    <>
+    <Header image={'https://sejadev-web-app-public-files.s3.amazonaws.com/courses/coffee2.jpg'}>
+     <Title>Outro Café Dev</Title>
+     <Subtitle text="O segundo melhor café para p programador"/>
+    </Header>
+   </>
+  )
+}
+
+export default OutroExemploPage
+
